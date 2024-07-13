6d0208f994980eef545a72aa4ddd797b290ceb41
Hasnain Muavia <131387615+HasnainMuavia1@users.noreply.github.com>
2024-07-13 21:08:11 +0500

    Update settings.py

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hasnain.muavia2004@gmail.com'
-EMAIL_HOST_PASSWORD = 'zsqi rgaj gkmb iwod  '
+EMAIL_HOST_USER = '<email>'
+EMAIL_HOST_PASSWORD = '<password>'
 
 # settings.py
 
